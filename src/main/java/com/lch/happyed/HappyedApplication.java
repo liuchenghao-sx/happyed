d2e3f8593f12ebb5972e8a8fb89262665c1eb85b
liuchenghao <liuchenghao@hd123.com>
2018-12-24 15:36:01 +0800

    LCH First

---
+++ src/main/java/com/lch/happyed/HappyedApplication.java
@@ -0,0 +1,14 @@
+package com.lch.happyed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyedApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HappyedApplication.class, args);
+  }
+
+}
+
